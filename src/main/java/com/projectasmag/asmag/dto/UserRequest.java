efb7e4e5f431d1d6630733d3362202cad806009f
Alex <romian37@gmail.com>
2026-01-11 20:34:25 +0700

    Add simple DTO

--- src/main/java/com/projectasmag/asmag/dto/UserRequest.java
+++ src/main/java/com/projectasmag/asmag/dto/UserRequest.java
@@ -1,4 +1,10 @@
 package com.projectasmag.asmag.dto;
 
 public class UserRequest {
+    private String employeeCode;
+    private String code;
+    private String email;
+    private String password;
+    private String roleCode;
+    private Integer createdBy;
 }
--- src/main/java/com/projectasmag/asmag/dto/UserResponse.java
+++ src/main/java/com/projectasmag/asmag/dto/UserResponse.java
@@ -1,4 +1,9 @@
 package com.projectasmag.asmag.dto;
 
+import com.projectasmag.asmag.model.company.Role;
+
 public class UserResponse {
+    private String code;
+    private String email;
+    private Role role;
 }
