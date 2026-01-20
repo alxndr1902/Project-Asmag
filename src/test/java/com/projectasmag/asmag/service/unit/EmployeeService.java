e597476a72e335ef0bb5fe9e94a163d186a048e8
Alexander <romian37@gmail.com>
2026-01-20 16:17:54 +0700

    create test unit in service layer

---
+++ src/test/java/com/projectasmag/asmag/service/unit/EmployeeService.java
@@ -0,0 +1,107 @@
+package com.projectasmag.asmag.service.unit;
+
+import com.projectasmag.asmag.dto.employee.CreateEmployeeRequestDTO;
+import com.projectasmag.asmag.dto.employee.UpdateEmployeeRequestDTO;
+import com.projectasmag.asmag.model.company.Company;
+import com.projectasmag.asmag.model.company.Employee;
+import com.projectasmag.asmag.repository.CompanyRepository;
+import com.projectasmag.asmag.repository.EmployeeRepository;
+import com.projectasmag.asmag.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeService {
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @Mock
+    private CompanyRepository companyRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    @Test
+    public void shouldCreated_whenDataValid() {
+        var employeeDto = new CreateEmployeeRequestDTO();
+        employeeDto.setCompanyId("77c1050a-602e-468a-ac0b-e1295fe62693");
+
+        var id = UUID.fromString("77c1050a-602e-468a-ac0b-e1295fe62691");
+
+        var employeeSaved = new Employee();
+        employeeSaved.setId(id);
+        employeeSaved.setCreatedAt(LocalDateTime.now());
+        employeeSaved.setCreatedBy(UUID.randomUUID());
+        employeeSaved.setVersion(0);
+
+        Mockito.when(companyRepository.findById(Mockito.any())).thenReturn(Optional.of(new Company()));
+        Mockito.when(employeeRepository.existsByPhoneNumber(Mockito.any())).thenReturn(true);
+        Mockito.when(employeeRepository.existsByIdentificationNumber(Mockito.any())).thenReturn(true);
+        Mockito.when(employeeRepository.save(Mockito.any())).thenReturn(employeeSaved);
+
+        var result = employeeService.createEmployee(employeeDto);
+
+        Assertions.assertEquals(id, result.id());
+        Mockito.verify(companyRepository, Mockito.atLeast(1)).findById(Mockito.any());
+        Mockito.verify(employeeRepository, Mockito.atLeast(1)).existsByPhoneNumber(Mockito.any());
+        Mockito.verify(employeeRepository, Mockito.atLeast(1)).existsByIdentificationNumber(Mockito.any());
+        Mockito.verify(employeeRepository, Mockito.atLeast(1)).save(Mockito.any());
+    }
+
+    @Test
+    public void shouldReturnData_whenIdValid() {
+        var id = UUID.randomUUID();
+        var employeeSaved = new Employee();
+        employeeSaved.setId(id);
+        employeeSaved.setFullName("John Doe");
+
+        Mockito.when(employeeRepository.findById(Mockito.any())).
+                thenReturn(Optional.ofNullable(employeeSaved));
+
+        var result = employeeService.getEmployee(id.toString());
+
+        Assertions.assertEquals("John Doe", result.fullName());
+        Mockito.verify(employeeRepository, Mockito.atLeast(1))
+                .findById(Mockito.any());
+    }
+
+    @Test
+    public void shouldUpdateVersion_whenDataIsValid() {
+        var id = UUID.randomUUID();
+
+        var employeeDTO = new UpdateEmployeeRequestDTO();
+        employeeDTO.setVersion(0);
+
+        var savedEmployee = new Employee();
+        savedEmployee.setPhoneNumber("123");
+        savedEmployee.setId(id);
+        savedEmployee.setVersion(1);
+
+        Employee employeeById = new Employee();
+        employeeById.setPhoneNumber("09");
+        employeeById.setVersion(0);
+
+        Mockito.when(employeeRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(employeeById));
+        Mockito.when(employeeRepository.saveAndFlush(Mockito.any())).thenReturn(savedEmployee);
+        Mockito.when(employeeRepository.findByPhoneNumber(Mockito.any())).thenReturn(Optional.of(savedEmployee));
+
+        var result =  employeeService.updateEmployee(id.toString(), employeeDTO);
+
+        Assertions.assertEquals(1, result.version());
+
+
+        Mockito.verify(employeeRepository, Mockito.atLeast(1))
+                .findById(Mockito.any());
+        Mockito.verify(employeeRepository, Mockito.atLeast(1))
+                .saveAndFlush(Mockito.any());
+    }
+}
---
+++ src/test/java/com/projectasmag/asmag/service/unit/LoanService.java
@@ -0,0 +1,118 @@
+package com.projectasmag.asmag.service.unit;
+
+import com.projectasmag.asmag.dto.loan.CreateLoanRequestDTO;
+import com.projectasmag.asmag.dto.loan.LoanDetailResponseDTO;
+import com.projectasmag.asmag.model.asset.Asset;
+import com.projectasmag.asmag.model.company.Employee;
+import com.projectasmag.asmag.model.company.Location;
+import com.projectasmag.asmag.model.loan.Loan;
+import com.projectasmag.asmag.model.loan.LoanDetail;
+import com.projectasmag.asmag.repository.*;
+import com.projectasmag.asmag.service.impl.LoanServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+@ExtendWith(MockitoExtension.class)
+public class LoanService {
+    @Mock
+    private LoanRepository loanRepository;
+
+    @Mock
+    private LoanDetailRepository loanDetailRepository;
+
+    @Mock
+    private AssetRepository assetRepository;
+
+    @Mock
+    private LocationRepository locationRepository;
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private LoanServiceImpl loanService;
+
+    @Test
+    public void shouldReturnAll() {
+        UUID loanId = UUID.randomUUID();
+        var loan = new Loan();
+        loan.setId(loanId);
+        List<Loan> loans = List.of(loan);
+
+        Mockito.when(loanRepository.findAll()).thenReturn(loans);
+
+        var result = loanService.getLoans();
+
+        Assertions.assertEquals(loans.size(), result.size());
+        Assertions.assertEquals(loans.get(0).getId(), result.get(0).id());
+
+        Mockito.verify(loanRepository, Mockito.times(1)).findAll();
+    }
+
+    @Test
+    public void shouldReturnData_whenIdValid() {
+        var loanId = UUID.randomUUID();
+        List<LoanDetail> loanDetails = new ArrayList<>();
+        var savedLoan = new Loan();
+        savedLoan.setId(loanId);
+        savedLoan.setLoanDetails(loanDetails);
+
+        Mockito.when(loanRepository.findById(loanId)).thenReturn(Optional.of(savedLoan));
+
+        Set<LoanDetailResponseDTO> result = loanService.getLoanById(loanId.toString());
+
+        Assertions.assertEquals(savedLoan.getLoanDetails().size(), result.size());
+    }
+
+    @Test
+    public void shouldCreated_whenDataValid() {
+        var targetId = UUID.randomUUID();
+        var assetId = UUID.randomUUID();
+
+        var loanDto = new CreateLoanRequestDTO();
+
+        loanDto.setLocationTargetId(targetId.toString());
+        loanDto.setAssetIdList(List.of(assetId.toString()));
+
+        var loanId = UUID.randomUUID();
+        var code = generateRandomAlphaNumeric();
+
+        var savedLoan = new Loan();
+        savedLoan.setId(loanId);
+        savedLoan.setCode(code);
+
+        Mockito.when(locationRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(new Location()));
+        Mockito.when(assetRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(new Asset()));
+        Mockito.when(loanRepository.save(Mockito.any())).thenReturn(savedLoan);
+        Mockito.when(loanDetailRepository.existsByAssetAndLoan(Mockito.any(), Mockito.any())).thenReturn(false);
+
+        var result = loanService.createLoan(loanDto);
+
+        Assertions.assertEquals(code, result.code());
+        Assertions.assertEquals(loanId, result.id());
+
+        Mockito.verify(assetRepository, Mockito.times(1)).findById(Mockito.any());
+        Mockito.verify(locationRepository, Mockito.atLeast(1)).findById(Mockito.any());
+        Mockito.verify(loanDetailRepository, Mockito.atLeast(1))
+                        .saveAll(Mockito.any());
+        Mockito.verify(loanRepository, Mockito.atLeast(1)).save(Mockito.any());
+    }
+
+    private String generateRandomAlphaNumeric() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder result = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            int index = random.nextInt(chars.length());
+            result.append(chars.charAt(index));
+        }
+        return result.toString();
+    }
+}
